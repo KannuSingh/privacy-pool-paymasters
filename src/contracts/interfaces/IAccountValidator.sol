f6b5a894999deaf1c951c8f5b2085c9b0eaf902e
Karandeep Singh <karandeepsinghcaj@outlook.com>
2025-08-12 16:20:52 -0400

    chore: update copyright year from 2024 to 2025

--- src/contracts/interfaces/IAccountValidator.sol
+++ src/contracts/interfaces/IAccountValidator.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright 2024 Karandeep Singh (https://github.com/KannuSingh)
+// Copyright 2025 Karandeep Singh (https://github.com/KannuSingh)
 pragma solidity 0.8.28;
 
 /**
